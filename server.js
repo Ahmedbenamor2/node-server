42d042c42bbe9faaf6f9bbeb39675dbcd969226b
ahmedbenamor2 <ahmedbenamar123456@gmail.com>
2024-01-12 23:18:35 +0100

    second_commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const xmlFilePath = 'incidents.xml';
 app.use(cors());
 app.use(bodyParser.json());
 
-// Load incidents from XML file on server start
+
 let incidents = [];
 
 try {
@@ -25,7 +25,7 @@ try {
   console.error('Error reading XML file:', error);
 }
 
-// Routes
+
 app.get('/incidents', (req, res) => {
   res.json(incidents);
 });
@@ -34,7 +34,7 @@ app.post('/incidents', (req, res) => {
   const newIncident = req.body;
   incidents.push(newIncident);
 
-  // Save incidents to XML file
+
   const builder = new xml2js.Builder();
   const xmlData = builder.buildObject({ incidents: { incident: incidents } });
 
@@ -47,7 +47,6 @@ app.post('/incidents', (req, res) => {
   res.status(201).json(newIncident);
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
